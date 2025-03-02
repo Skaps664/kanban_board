14a81c2b4c636c99aa112f5ce68f623094f660a7
Skaps664 <msudaisk664@gmail.com>
2025-03-02 06:10:18 +0500

    machine change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        mainBackgroundColor: '#0D1117',
-        columnBackgroundColor: '#161C22'
-      }
+        mainBackgroundColor: "#0D1117",
+        columnBackgroundColor: "#161C22",
+      },
     },
   },
   plugins: [],
-}
-
+};
